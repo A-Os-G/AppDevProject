abe3493525ee62ea1f2a785625f0868a80cd0981
A-Os-G <abdulrahman3113osama@gmail.com>
2023-11-11 22:25:17 +0800

    Final touches on welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,16 +22,16 @@
         <div class="sm:flex">
 
             <!--left-->
-            <div class="flex flex-col w-7/12 p-4 bg-s h-screen items-center sm:justify-center">
+            <div class="flex flex-col w-7/12 bg-s h-screen items-center sm:justify-center">
 
                 <!--logo-->
-                <div class="overflow-hidden h-4/12 p-8 flex sm:justify-center">
-                    <img src="pics/logo.jpg" alt="b" class="m-9 h-48 w-72 max-w-min" style="border-top-left-radius: 35%; border-top-right-radius: 35%">
+                <div class="overflow-hidden flex sm:justify-center drop-shadow-xl">
+                    <img src="pics/logo.jpg" alt="b" class=" w-8/12 max-w-min mb-8 p-1" style="border-top-left-radius: 35%; border-top-right-radius: 35%">
                 </div>
 
                 <!--bottom pic-->
-                <div class="overflow-hidden h-8/12 pb-5 flex sm:justify-center">
-                    <img src="pics/background.jpg" alt="second img" class="w-2/3" style="border-top-left-radius: 35%; border-top-right-radius: 35%">
+                <div class="overflow-hidden flex sm:justify-center drop-shadow-xl">
+                    <img src="pics/background.jpg" alt="second img" class="w-3/5 max-w-min" style="border-top-left-radius: 35%; border-top-right-radius: 35%">
                 </div>
 
             </div>
@@ -60,39 +60,37 @@
 >>>>>>> 7883a66e2d48d984ba8c05ace8182c53f7486be4
                 </div>
 
-                <hr class="w-80 border-s my-4 border-opacity-25 border-2">
+                <hr class="w-80 border-s border-2">
 
-                <div class="flex items-center justify-center p-4 w-100 hover:text-m hover:bg-s bg-m text-s text-xl font-extrabold">
-                    <button class="w-80">
-                        <a href="{{ route('register') }}" class="" wire:navigate>
-                            {{__('Register')}}
-                        </a>
-                    </button>
-                </div>
-
-                <hr class="w-80 border-s my-4 border-opacity-25 border-2">
-
-                <div class="flex items-center justify-center p-4 w-100 hover:text-m hover:bg-s bg-m text-s text-xl font-extrabold">
-                    <button class="w-80">
-                        <a href="{{ route('login') }}" class="w-full" wire:navigate>
-                            {{__('Shop')}}
-                        </a>
-                    </button>
-                </div>
-
-                <hr class="w-80 border-s my-4 border-opacity-25 border-2">
-
-                <div class="flex items-center justify-center p-4 w-100 hover:text-m hover:bg-s bg-m text-s text-xl font-extrabold">
-                    <button class="w-80">
-                        <a href="{{ route('login') }}" class="w-full" wire:navigate>
+                <div class="flex items-center justify-center hover:text-m hover:bg-s bg-m text-s text-xl font-extrabold rounded-md">
+                    <a href="{{ route('login') }}" wire:navigate>
+                <button class="w-64 py-2 rounded">
                             {{__('Cart')}}
-                        </a>
-                    </button>
+                        </button>
+                    </a>
                 </div>
+                <hr class="w-80 border-s border-2">
 
-                <hr class="w-80 border-s my-4 border-opacity-25 border-2">
+                <div class="flex items-center justify-center hover:text-m hover:bg-s bg-m text-s text-xl font-extrabold rounded-md">
+                    <a href="{{ route('login') }}" class="w-full" wire:navigate>
+                <button class="w-64 py-2 rounded">
+                            {{__('Login')}}
+                        </button>
+                    </a>
+                </div>
+                <hr class="w-80 border-s border-2">
+
+                <div class="flex items-center justify-center hover:text-m hover:bg-s bg-m text-s text-xl font-extrabold rounded-md">
+                    <a href="{{ route('register') }}" class="w-full" wire:navigate>
+                    <button class="w-64 py-2 rounded">
+                            {{__('Register')}}
+                        </button>
+                    </a>
+                </div>
+                <hr class="w-80 border-s border-2">
 
             </div>
+
         </div>
     </body>
 </html>
