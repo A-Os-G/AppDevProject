56d8b4bcb1f3840f74c2c9b1422c0be1d130842d
Almond18 <M.zufar000@gmail.com>
2023-11-11 19:16:19 +0800

    Updated welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,24 +17,26 @@
         <div class="sm:flex">
 
             <!--left-->
-            <div class="flex flex-col w-8/12 p-4 bg-s h-screen items-center sm:justify-center">
+            <div class="flex flex-col w-7/12 p-4 bg-s h-screen items-center sm:justify-center">
 
                 <!--logo-->
                 <div class="overflow-hidden h-4/12 p-8 flex sm:justify-center">
-                    <img src="pics/logo.jpg" alt="b" class="m-9 h-auto w-96 max-w-min" style="border-top-left-radius: 35%; border-top-right-radius: 35%">
+                    <img src="pics/logo.jpg" alt="b" class="m-9 h-48 w-72 max-w-min" style="border-top-left-radius: 35%; border-top-right-radius: 35%">
                 </div>
 
                 <!--bottom pic-->
                 <div class="overflow-hidden h-8/12 pb-5 flex sm:justify-center">
-                    <img src="pics/background.jpg" alt="second img" class="w-10/12" style="border-top-left-radius: 35%; border-top-right-radius: 35%">
+                    <img src="pics/background.jpg" alt="second img" class="w-2/3" style="border-top-left-radius: 35%; border-top-right-radius: 35%">
                 </div>
                 
             </div>
 
             <!--Right-->
-            <div class="flex flex-col w-4/12 bg-m space-y-8 items-center justify-center h-screen">
-                
-                <div class="flex items-center justify-center p-4 w-90 hover:text-m hover:bg-s bg-m text-s text-xl font-extrabold border-2 border-b-0 border-l-0 border-r-0 border-s border-opacity-30">
+            <div class="flex flex-col w-5/12 bg-m space-y-3 items-center justify-center h-screen">
+
+                <hr class="w-80 border-s my-4 border-opacity-25 border-2">
+
+                <div class="flex items-center justify-center p-4 w-100 hover:text-m hover:bg-s bg-m text-s text-xl font-extrabold">
                     <button class="w-80">
                         <a href="{{ route('login') }}" class="w-full" wire:navigate>
                             {{__('Log in')}}
@@ -42,7 +44,9 @@
                     </button>
                 </div>
 
-                <div class="flex items-center justify-center p-4 w-110 hover:text-m hover:bg-s bg-m text-s text-xl font-extrabold border-2 border-b-0 border-l-0 border-r-0 border-s border-opacity-30">
+                <hr class="w-80 border-s my-4 border-opacity-25 border-2">
+
+                <div class="flex items-center justify-center p-4 w-100 hover:text-m hover:bg-s bg-m text-s text-xl font-extrabold">
                     <button class="w-80">
                         <a href="{{ route('register') }}" class="" wire:navigate>
                             {{__('Register')}}
@@ -50,7 +54,9 @@
                     </button>
                 </div>
 
-                <div class="flex items-center justify-center p-4 w-100 hover:text-m hover:bg-s bg-m text-s text-xl font-extrabold border-2 border-b-0 border-l-0 border-r-0 border-s border-opacity-30">
+                <hr class="w-80 border-s my-4 border-opacity-25 border-2">
+
+                <div class="flex items-center justify-center p-4 w-100 hover:text-m hover:bg-s bg-m text-s text-xl font-extrabold">
                     <button class="w-80">
                         <a href="{{ route('login') }}" class="w-full" wire:navigate>
                             {{__('Shop')}}
@@ -58,13 +64,17 @@
                     </button>
                 </div>
 
-                <div class="flex items-center justify-center p-4 w-90 hover:text-m hover:bg-s bg-m text-s text-xl font-extrabold border-2 border-l-0 border-r-0 border-s border-opacity-30">
-                    <button class="w-80 pb-8">
+                <hr class="w-80 border-s my-4 border-opacity-25 border-2">
+
+                <div class="flex items-center justify-center p-4 w-100 hover:text-m hover:bg-s bg-m text-s text-xl font-extrabold">
+                    <button class="w-80">
                         <a href="{{ route('login') }}" class="w-full" wire:navigate>
                             {{__('Cart')}}
                         </a>
                     </button>
                 </div>
+
+                <hr class="w-80 border-s my-4 border-opacity-25 border-2">
 
             </div>
         </div>
