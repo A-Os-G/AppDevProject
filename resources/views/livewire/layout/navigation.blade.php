a44f6a2c0568e0f86cb46be5c9646bf02780a359
A-Os-G <abdulrahman3113osama@gmail.com>
2023-12-26 20:23:00 +0800

    fix logo disappearing

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -23,7 +23,7 @@ new class extends Component
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="/dashboard">
-                        <img src="pics/logo.jpg" class="block h-16 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <img src="/pics/logo.jpg" class="block h-16 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
